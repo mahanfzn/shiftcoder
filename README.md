ca9fedff16f5604cbb778100c58527b804645071
Mahan Forouzande <mahan.forouzandehnick@gmail.com>
2021-04-09 06:55:14 +0430

    push in github

---
+++ README.md
@@ -0,0 +1,1 @@
+# shiftcoder
---
+++ scripts/script-login-form.js
@@ -0,0 +1,37 @@
+function checkinput(a){
+    var txt= document.getElementById(a);
+    if(txt.value==""){
+        txt.style.borderColor="red";
+    }
+    else{
+        txt.style.borderColor="green";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
