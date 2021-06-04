111145e84483ee605f59bc84d74276326d2ef129
Abbas._.dh <abbasdh900@gmail.com>
2021-06-04 22:22:27 +0430

    new save have lot of bog but we will fix them

--- scripts/singUp.js
+++ scripts/singUp.js
@@ -1,153 +1,94 @@
 const send = new meesage()
-
 // vareble 
-
-
-const usersName = document.querySelector('#usersName'),
-    email = document.querySelector('#email-input'),
-    phone = document.querySelector('#phone'),
-    password = document.querySelector('#password-input'),
-    reapetPass = document.querySelector('#reapetPass'),
-    submit = document.querySelector('#submit'),
-    form = document.querySelector('#form')
-
-
-
+let validation = document.querySelectorAll('.validation')
+let form = document.querySelector('#form')
+let submit = document.querySelector('#submit')
+let spiner = document.querySelector('#spiner')        
 // everntLiseteaner
 eventListeaner()
 function eventListeaner() {
-    document.addEventListener('DOMContentLoaded', loadPage)
-    usersName.addEventListener('blur', sendData)
-    email.addEventListener('blur', sendData)
-    phone.addEventListener('blur', sendData)
-    password.addEventListener('blur', sendData)
-    reapetPass.addEventListener('blur', sendData)
+    validation[0].addEventListener('blur', ValidTheName); //name
+    validation[1].addEventListener('blur', ValidTheeamil)//email
+    validation[2].addEventListener('blur', ValidThePhone)//phone
+    validation[3].addEventListener('blur', ValidThePassword)//password
+    validation[4].addEventListener('blur', sendData)//reapetThePass
     form.addEventListener('submit', (e) => {
         e.preventDefault()
-        const spiner = document.querySelector('#spiner')
-        spiner.style.display = 'block'
+        spiner.style.display = "block"
+        console.log(spiner)
         setTimeout(() => {
             spiner.style.display = 'none'
-
         }, 3000);
     })
 }
-
-function loadPage() {
-
-    submit.disabled = true
-}
 function sendData() {
     // check the uses Data
-    if (usersName.value != '' && email.value != '' && phone.value != '' && password.value != '') {
-        ValidTheName(usersName.value)
-        ValidTheeamil(email.value)
-        ValidThePhone(phone.value)
-        ValidThePassword(password.value)
-        ValidTheReapetPass(password.value, reapetPass.value)
+    if (validation[3].value != '' && validation[4].value != '') {
+        ValidTheReapetPass(validation[3].value, validation[4].value)
+    }
+    const Error = document.querySelectorAll('.error')
+    if (Error.length == 0) {
+        submit.style.cursor = 'pointer'
+        validation[5].disabled = false
 
-
-        const error = document.querySelectorAll('.error')
-        if (error.length == 0) {
-            submit.disabled = false
-
-
-
-        }
     }
 }
-
-
-
-
 // valid the name
 function ValidTheName(name) {
-    if (name.length >= 3) {
-        usersName.style.borderColor = "green"
-        submit.classList.remove('error')
-
+    if (name.target.value !== '' && name.target.value.length >= 3) {
+        validation[0].style.borderColor = "green"
+        validation[5].classList.remove('error')
     } else {
-        submit.classList.add('error')
-        send.sendMessage('لطفا اسم خود را صحیح وارد کنید', 'aler alert-danger text-center')
-        usersName.style.borderColor = "red"
-
+        validation[5].classList.add('error')
+        send.sendMessage('لطفا اسم خود را صحیح وارد کنید', 'aler alert-danger text-center', 'meesageName')
+        validation[0].style.borderColor = "red"
     }
 }
-
 // valid the eamil
 function ValidTheeamil(getEmail) {
-    if (getEmail.length >= 13 && getEmail.includes("@gmail.com")) {
-        email.style.borderColor = "green"
-        submit.classList.remove('error')
-
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (getEmail.target.value.match(mailformat) && getEmail.target.value !== '') {
+        validation[1].style.borderColor = "green"
+        validation[5].classList.remove('error')
     } else {
-        submit.classList.add('error')
-        send.sendMessage('لطفا ایمیل خود را صحیح وارد کنید', 'aler alert-danger text-center')
-        email.style.borderColor = "red"
-
+        validation[1].style.borderColor = "red"
+        validation[5].classList.add('error')
+        send.sendMessage('لطفا ایمیل خود را صحیح وارد کنید', 'aler alert-danger text-center', 'meesageEmail')
     }
 }
-
-// // valid the phone
-
+ // valid the phone
 function ValidThePhone(getPhone) {
-    // check the number length
-    if (getPhone.length == 11) {
-        var convert = parseInt(getPhone)
-        if (typeof convert == 'number') {
-            phone.style.borderColor = "green"
-            submit.classList.remove('error')
-
-        }
+    var phoneFormat = /09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}/;
+    if (getPhone.target.value.match(phoneFormat)) {
+        validation[2].style.borderColor = "green"
+        validation[5].classList.remove('error')
     } else {
-        submit.classList.add('error')
-        send.sendMessage('لطفا شماره خود را صحیح وارد کنید', 'aler alert-danger text-center')
-        phone.style.borderColor = "red"
-
-
+        validation[5].classList.add('error')
+        send.sendMessage('لطفا شماره خود را صحیح وارد کنید', 'aler alert-danger text-center', 'meesagePhone')
+        validation[2].style.borderColor = "red"
     }
-
 }
-
-// valid the password
+// // valid the password
 function ValidThePassword(pass) {
-    if (pass.length >= 5) {
-        password.style.borderColor = "yellow"
-        submit.classList.remove('error')
-        if (pass.length >= 10) {
-            password.style.borderColor = "green"
-            submit.classList.remove('error')
-
-        }
-
-    } else {
-        submit.classList.add('error')
-        send.sendMessage('لطفا رمز خود را وارد کنید', 'aler alert-danger text-center')
-        password.style.borderColor = "red"
-
-
-
+    if (pass.target.value !== '' && pass.target.value.length >= 8) {
+        validation[3].style.borderColor = "green"
+        validation[5].classList.remove('error')
     }
-
-
-}
-// valid the reaoetPass
-function ValidTheReapetPass(pass, rpPass) {
-
-    if (rpPass === pass) {
-        reapetPass.style.borderColor = "green"
-        submit.classList.remove('error')
-
-
-
-
-    } else {
-        submit.classList.add('error')
-        send.sendMessage('تکرارگذرواژه با گذرواژه بابر نیست', 'aler alert-danger text-center')
-        reapetPass.style.borderColor = "red"
-
-
-
+     else {
+        validation[5].classList.add('error')
+        send.sendMessage('لطفا رمز خود را وارد کنید', 'aler alert-danger text-center', 'meesagePassword')
+        validation[3].style.borderColor = "red"
     }
 }
-
+// // valid the reapetPass
+function ValidTheReapetPass(pass, reapPass) {
+    if (pass === reapPass) {
+        validation[4].style.borderColor = "green"
+        validation[5].classList.remove('error')
+    } else {
+        validation[5].classList.add('error')
+        send.sendMessage('تکرارگذرواژه با گذرواژه بابر نیست', 'aler alert-danger text-center', 'meesageRepass')
+        validation[4].style.borderColor = "red"
+        console.log(validation[5]);
+    }
+}
